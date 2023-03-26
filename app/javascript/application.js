23b0e98a42aa0da2625e480467d3e895eb1fc18c
tonycarballeira <tonycarballeira@gmail.com>
2023-03-26 19:55:14 -0400

    updated migrations

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
+Turbo.session.drive = true
 import "controllers"
+import 'jquery'
+
+import "@hotwired/turbo-rails"
+
